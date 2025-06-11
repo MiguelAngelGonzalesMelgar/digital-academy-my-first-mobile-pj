a56da8e0e60e011128710f04ceae1b2855bca631
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-11 12:45:45 -0400

    fix: update dotenv plugin.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
-module.exports = [
-  'module:react-native-dotenv',
-  {
-    moduleName: '@env',
-    path: '.env',
-  },
-];
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
+};
