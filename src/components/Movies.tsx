0fb3cc9f164f1d4c58d7a6006c833614ca9f8494
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-13 11:33:35 -0400

    refactor: modify Movies component to handle optional title rendering.

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,30 +1,20 @@
 import {FlatList, Image, StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-gesture-handler';
 
 const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 10,
-  },
-  image: {
-    width: 180,
-    height: 250,
-    margin: 5,
-    borderRadius: 8,
-  },
-});
 
 export interface Movie {
   id: string;
   poster_path: string;
+  title?: string;
 }
 
 interface MoviesProps {
   movies: Movie[];
+  showMovieTitle?: boolean;
 }
 
-const Movies = ({movies}: MoviesProps) => {
+const Movies = ({movies, showMovieTitle = true}: MoviesProps) => {
   return (
     <View style={styles.container}>
       <FlatList
@@ -32,13 +22,50 @@ const Movies = ({movies}: MoviesProps) => {
         keyExtractor={item => item.id.toString()}
         horizontal
         renderItem={({item}) => (
-          <Image
-            style={styles.image}
-            source={{uri: `${POSTER_BASE_URL}${item.poster_path}`}}
-          />
+          <View style={styles.movieItemContainer}>
+            <Image
+              style={styles.image}
+              source={{uri: `${POSTER_BASE_URL}${item.poster_path}`}}
+              defaultSource={{
+                uri: 'https://placehold.co/150x200/cccccc/000000?text=No+Image',
+              }}
+              onError={error =>
+                console.log('Error cargando imagen:', error.nativeEvent.error)
+              }
+            />
+            {showMovieTitle && item.title && (
+              <Text style={styles.movieTitle}>{item.title}</Text>
+            )}
+          </View>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  movieItemContainer: {
+    marginHorizontal: 10,
+    alignItems: 'center',
+    width: 150,
+  },
+  image: {
+    width: 150,
+    height: 200,
+    marginHorizontal: 10,
+    borderRadius: 8,
+  },
+  movieTitle: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'semibold',
+    textAlign: 'left',
+    marginVertical: 10,
+  },
+});
+
 export default Movies;
