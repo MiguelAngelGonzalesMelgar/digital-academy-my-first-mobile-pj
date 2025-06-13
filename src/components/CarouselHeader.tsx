1667d8b9e006b51cfb2e1f7c40e80d5cf8e53379
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-13 09:58:05 -0400

    feature: add CarouselHeader reusable component.

---
+++ src/components/CarouselHeader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+interface CarouselHeaderProps {
+  title: string;
+  onLinkPress: () => void;
+}
+
+const CarouselHeader = ({title, onLinkPress}: CarouselHeaderProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>{title}</Text>{' '}
+      <TouchableOpacity
+        onPress={onLinkPress}
+        style={styles.linkButton}
+        accessibilityRole="link"
+        accessibilityLabel={`See more`}>
+        <Text style={styles.linkText}>See More</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+  linkButton: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  linkText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#F3C15D',
+  },
+});
+
+export default CarouselHeader;
