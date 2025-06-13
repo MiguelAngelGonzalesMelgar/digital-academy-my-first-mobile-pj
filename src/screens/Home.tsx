ff272cb0c222cf19fb4827989b102e56f862eed9
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-13 14:44:59 -0400

    refactor: group repeated headers into a variable headers and replace useEffect to handle 2 endpoint calls using promiseAll.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import {ActivityIndicator, ScrollView, StyleSheet, View} from 'react-native';
-import {getBestMovies} from '../utils/service/TMDBService';
+import {getBestMovies, getMarvelMovies} from '../utils/service/TMDBService';
 import Movies, {Movie} from '../components/Movies';
 import Slider from '../components/Slider';
 import CarouselHeader from '../components/CarouselHeader';
 
 const Home = () => {
   const [bestMovies, setBestMovies] = useState<Movie[]>([]);
-  const [marvelMovies, setMarvelBestmovies] = useState<Movie[]>([]);
+  const [marvelMovies, setMarvelMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
-    getBestMovies()
-      .then(data => {
-        setBestMovies(data || []);
-      })
-      .finally(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      try {
+        const [bestMoviesData, marvelMoviesData] = await Promise.all([
+          getBestMovies(),
+          getMarvelMovies(),
+        ]);
+
+        setBestMovies(bestMoviesData || []);
+        setMarvelMovies(marvelMoviesData || []);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    };
+
+    fetchMovies();
   }, []);
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <ScrollView contentContainerStyle={{flexGrow: 1, paddingBottom: 40}}>
         <Slider />
         <View style={styles.contentContainer}>
           {isLoading ? (
@@ -29,7 +41,7 @@ const Home = () => {
           ) : (
             <>
               <CarouselHeader title="Marvel Studios" onLinkPress={() => {}} />
-              <Movies movies={bestMovies} />
+              <Movies movies={marvelMovies} />
               <CarouselHeader title="Best movies" onLinkPress={() => {}} />
               <Movies movies={bestMovies} showMovieTitle={false} />
             </>
--- src/utils/service/TMDBService.ts
+++ src/utils/service/TMDBService.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import {TMDB_ACCESS_TOKEN, TMDB_BASE_URL} from '@env';
 
+const headers = {
+  Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+  'Content-Type': 'application/json',
+};
+
 export const getRecentMovies = async () => {
   try {
     const response = await axios.get(`${TMDB_BASE_URL}/movie/now_playing`, {
-      headers: {
-        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       params: {
         language: 'en-US',
       },
@@ -15,19 +17,17 @@ export const getRecentMovies = async () => {
     return response.data.results;
   } catch (error: any) {
     console.error(
-      'Cannot get popular movies:',
+      'Cannot get recent movies:',
       error.response?.data || error.message || error,
     );
     throw error;
   }
 };
+
 export const getBestMovies = async () => {
   try {
     const response = await axios.get(`${TMDB_BASE_URL}/movie/top_rated`, {
-      headers: {
-        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       params: {
         language: 'en-US',
         pages: 2,
@@ -36,19 +36,17 @@ export const getBestMovies = async () => {
     return response.data.results;
   } catch (error: any) {
     console.error(
-      'Cannot get popular movies:',
+      'Cannot get best movies:',
       error.response?.data || error.message || error,
     );
     throw error;
   }
 };
+
 export const getPopularMovies = async () => {
   try {
     const response = await axios.get(`${TMDB_BASE_URL}/movie/popular`, {
-      headers: {
-        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       params: {
         language: 'en-US',
       },
@@ -57,6 +55,27 @@ export const getPopularMovies = async () => {
   } catch (error: any) {
     console.error(
       'Cannot get popular movies:',
+      error.response?.data || error.message || error,
+    );
+    throw error;
+  }
+};
+
+export const getMarvelMovies = async () => {
+  try {
+    const marvelStudiosCompanyId = 420;
+
+    const response = await axios.get(`${TMDB_BASE_URL}/discover/movie`, {
+      headers: headers,
+      params: {
+        language: 'en-US',
+        with_companies: marvelStudiosCompanyId,
+      },
+    });
+    return response.data.results;
+  } catch (error: any) {
+    console.error(
+      'Cannot get Marvel movies:',
       error.response?.data || error.message || error,
     );
     throw error;
