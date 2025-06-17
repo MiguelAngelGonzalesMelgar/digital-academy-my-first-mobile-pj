574477c5e0667f1f3fb1ccae9f77ab021997dc05
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-17 08:58:33 -0400

    feature: add react navigation native, configure navigation and add stack.

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import Home from './src/screens/Home';
 import {MovieModalProvider} from './src/context/MovieModalContext';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Whishlist from './src/screens/Whishlist';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.mainContainer}>
-      <MovieModalProvider>
+      {/* <MovieModalProvider>
         <Home />
-      </MovieModalProvider>
+      </MovieModalProvider> */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Whishlist" component={Whishlist} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
