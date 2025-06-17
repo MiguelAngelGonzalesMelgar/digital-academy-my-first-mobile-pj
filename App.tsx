d4ec190f68c36e3fcd37f5ae286765a04406fd52
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-17 15:05:09 -0400

    chore: build basic route components.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
 import {MovieModalProvider} from './src/context/MovieModalContext';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigation';
-
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
--- src/screens/Whishlist.tsx
+++ src/screens/Whishlist.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
 const Whishlist = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://i0.wp.com/thehill.com/wp-content/uploads/sites/2/2022/05/e92ed228a6924420a8963bdc6c9ddef1.jpg?w=2000&ssl=1',
+      }}
+      style={styles.backgroundImage}>
+      <View style={styles.overlay} />
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Your Whishlist</Text>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 16,
+    marginTop: 32,
+  },
+  title: {
+    color: '#ffffff',
+    fontFamily: 'Gilroy-bold',
+    fontSize: 21,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 32,
+    borderRadius: 10,
+  },
+});
 
 export default Whishlist;
