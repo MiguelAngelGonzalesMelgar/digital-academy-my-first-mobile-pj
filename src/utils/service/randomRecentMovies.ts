a134663ebff71f0e3fc68ce7bea7c3dab7a55087
Miguel Angel Gonzales Melgar <miguelangel.gonzales@assuresoft.tech>
2025-06-18 15:43:53 -0400

    Enhance/fm 7 (#21)
    * chore: build basic route components.
    
    * chore: change config to add ion icons to android. Clean gradle.
    
    * refactor: change switch cases to a map object to handle navigation items.
    
    * feature: add custom hook useTMDB.
    
    * refactor: change the logic to render movies with custom hook and modify topRatedMovies utils function to handle random movie renders.

---
+++ src/utils/service/randomRecentMovies.ts
@@ -0,0 +1,6 @@
+const randomRecentMovies = <T>(array: T[], count: number): T[] => {
+  const shuffled = [...array].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+};
+
+export default randomRecentMovies;
--- src/utils/service/topRatedMovies.ts
+++
@@ -1,8 +0,0 @@
-interface Movies {
-  id: string;
-  vote_average: number;
-  poster_path: string;
-}
-export function getTopRatedMovies(movies: Movies[], minRating = 7, limit = 5) {
-  return movies.sort((a, b) => b.vote_average - a.vote_average).slice(0, limit);
-}
